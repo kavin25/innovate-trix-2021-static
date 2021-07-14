ba2beffee4a9f144c0f62313ac1da3dffc39d9a9
Kavin valli <kavinvalli@gmail.com>
2021-07-14 20:01:23 +0530

    feat: Add Target Customer Analysis to product page

---
+++ components/product/TargetCustomer.js
@@ -0,0 +1,60 @@
+function TargetCustomer() {
+  return (
+    <div>
+      <h2>Target Customer Analysis</h2>
+      <p>
+        A comprehensive target market customer analysis is one of the crucial
+        components to help business with all the required information and making
+        wise business decisions. The 4 most basic yet the important questions
+        while formulating an analysis plan are:
+        <br />
+        <br />
+        <ul>
+          <li>
+            Who are the potential customers?
+            <br />
+            Since our product is healthcare-related, our customer base includes
+            every single individual in India which includes those who might be
+            in a state of emergency at any point of life or already suffers from
+            a disease or problem that might cause a mishap in the future. As per
+            the statistics available, India on an average receives 75,000
+            emergencies calls a day
+          </li>
+          <li>
+            Where are they located?
+            <br />
+            The best part of our product is that it is neither restricted to any
+            specific region of the country. Anyone present in any region of the
+            country can access the feature as long as there is a hospital
+            present in the nearby or surrounding areas.
+          </li>
+          <li>
+            How large is our target market?
+            <br />
+            As mentioned, our target market includes every single individual in
+            the country but going by the practicality that occurs in the
+            ordinary business of life, only 10% of the total customers aware of
+            the product actually avail it. This generally occurs in the case of
+            a luxury but we believe that our product instead of being a luxury
+            is actually a basic necessity. So, the rule of 10% customer
+            acquisition actually holds, as we believe, invalid in our case and
+            with that we would like to get back to the point where our target
+            market basically includes every single individual
+          </li>
+          <li>
+            What section of the society is being targeted and what income groups
+            can avail the features? The best part of our product is that it
+            never charges a single rupee from the customer, our business model
+            is to charge the hospitals to which the customers are redirected. In
+            case of private ones, a commission is charged on the hospital bill
+            of the patient while in the case of a government hospital, if
+            granted the permission to get the software (product) implemented, no
+            commission will be charged from the user or the hospital.
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default TargetCustomer;
--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,6 @@
 import BusinessPlan from "../components/product/BusinessPlan";
 import Functionality from "../components/product/Functionality";
+import TargetCustomer from "../components/product/TargetCustomer";
 import Vision from "../components/product/Vision";
 
 export default function Product() {
@@ -8,6 +9,7 @@ export default function Product() {
       <h1 className="mb-4">Product</h1>
       <Vision />
       <BusinessPlan />
+      <TargetCustomer />
       <Functionality />
     </main>
   );
